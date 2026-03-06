1288652f41ad4296e505cc31480ff29ddb5ac056
Ondrej <ondrejhlavac02@gmail.com>
2026-03-06 23:11:44 +0100

    Initial Commit

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\BookApiController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookRpcController;
+use App\Http\Controllers\BookSacController;
+use App\Http\Controllers\TimeRestController;
+use App\Http\Controllers\TimeRpcController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/time/rest', [TimeRestController::class, 'getTime']);
+Route::get('/time/rpc', [TimeRpcController::class, 'getTime']);
+Route::prefix('restapi')->group(function () {
+    Route::apiresource('books', BookApiController::class);
+});
+Route::get('/sac/books/{id}', BookSacController::class);
+Route::post('/rpc/books/{id}/borrow', [BookRpcController::class, 'borrowBook']);
+Route::post('/rpc/books/{id}/return', [BookRpcController::class, 'returnBook']);
